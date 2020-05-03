d5d3e54741012fc645f9fbaa61ca995e29040ff8
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-05-02 19:58:47 -0700

    commit6

--- script.js
+++ script.js
@@ -48,3 +48,33 @@ function showLoading() {
     }, 300);
   }, 1000);
 }
+
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
+// Show initial posts
+showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
